174331650adde24415094f1cc4832ee289280dee
chintochau <what.chau@gmail.com>
2024-10-15 02:17:32 -0400

    DEMO: play queue

--- src/renderer/src/components/CompactPlayer.jsx
+++ src/renderer/src/components/CompactPlayer.jsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useDevices } from '../context/devicesContext'
 import SyncStatus from './SyncStatus'
 import PlayStatus from './PlayStatus'
 import { useBrowsing } from '../context/borwsingContext'
 import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import { ScrollArea } from '@/components/ui/scroll-area'
+
+
+const demoPlayqueue = [
+  "Song 1",
+  "Song 2",
+  "Song 3",
+  "Song 4",
+  "Song 5",
+  "Song 6",
+  "Song 7",
+  "Song 8",
+  "Song 9",
+  "Song 10",
+  "Song 11",
+  "Song 12",
+  "Song 13",
+  "Song 14",
+  "Song 15",
+]
 
 const CompactPlayer = ({ ip }) => {
   const { devices } = useDevices()
   const { selectedPlayer, setSelectedPlayer } = useBrowsing()
   const device = devices.find((device) => device.ip === ip)
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   return (
     <div
       onClick={() => setSelectedPlayer(device)}
@@ -20,6 +43,20 @@ const CompactPlayer = ({ ip }) => {
       <div className="flex  w-full">
         <PlayStatus ip={ip} />
       </div>
+      <Button onClick={() => setIsMenuOpen(!isMenuOpen)} className="mt-4" variant="outline">
+        Queue
+      </Button>
+      <ScrollArea className={cn("flex w-full duration-300 ease-in flex-col", isMenuOpen ? "w-full h-96 " : "h-0 overflow-hidden")}>
+        {
+          demoPlayqueue.map((song, index) => (
+            <div key={index} className="flex flex-col w-full text-3xl">
+              <div className="flex items-center">
+                {index + 1}. {song}
+              </div>
+            </div>
+          ))
+        }
+      </ScrollArea>
     </div>
   )
 }
